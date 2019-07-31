844d9f13bdfeb022ae8244c620dcb1670c0812e8
Rohit K <rohit_kumar@rsystems.com>
2019-07-31 13:21:20 +0530

    project setup

---
+++ app/src/main/java/rohit5k2/awsauth/MainActivity.kt
@@ -0,0 +1,12 @@
+package rohit5k2.awsauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
